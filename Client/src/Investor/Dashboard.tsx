1f35742ee6d2ff50849d052ab3fe38912cb71506
Miyabi Nagumo <zebihaider123@gmail.com>
2025-05-20 16:57:31 +0500

    removed type errors

--- Client/src/Investor/Dashboard.tsx
+++ Client/src/Investor/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
 import { useState } from "react";
 import Sidebar from "../PublicComponents/Components/Sidebar";
--- Client/src/PublicComponents/Components/Unauthorized.tsx
+++ Client/src/PublicComponents/Components/Unauthorized.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Unauthorized = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
